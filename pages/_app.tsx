56481dce456e87be44cf0cde4d6de7d44f02fec6
Adrián Serrano <aserrano@ea.com>
2021-03-25 23:06:54 +0100

    Fix build issues for deployment test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  const a: string = 0
+function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
 
