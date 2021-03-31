5691fe2d8ca7449274711daf97234d8ad0b1f880
Adrián Serrano <aserrano@ea.com>
2021-03-31 11:44:14 +0200

    Fix theme bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,17 @@ import '../styles/globals.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { ThemeProvider } from 'next-themes'
+import { appWithTranslation } from 'next-i18next'
 
 library.add(fab)
 
 function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" defaultTheme="light">
       <Component {...pageProps} />
     </ThemeProvider>
   )
 }
 
-export default MyApp
+export default appWithTranslation(MyApp)
