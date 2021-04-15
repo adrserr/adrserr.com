592fbe74ad40f063dc87fbdf2a9982a67c66e69a
Adrián Serrano <aserrano@ea.com>
2021-04-15 21:05:36 +0200

    Optimize production build using Preact

--- next.config.js
+++ next.config.js
@@ -4,5 +4,15 @@ module.exports = {
   future: {
     webpack5: true
   },
+  webpack: (config, { isServer, dev }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      })
+    }
+    return config
+  },
   i18n
 }
