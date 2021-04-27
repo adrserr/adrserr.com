e6dfe7001ed47ccf9e6313e383f6983db8f31c9d
Adrián Serrano <aserrano@ea.com>
2021-04-27 23:27:01 +0200

    xhtml namespace

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -15,7 +15,7 @@ const prettier = require('prettier');
   // FIXME: Refactor
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
             ${pages
               .map((page) => {
                 if(!page.includes('pages')) {
