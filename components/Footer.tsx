416472b7ffd5acb1e381c602c2726df36733e1aa
Adrián Serrano <aserrano@ea.com>
2021-05-01 16:28:09 +0200

    Fix nowplaying not found

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,6 @@ import {
   RiLinkedinBoxFill,
   RiMailFill
 } from 'react-icons/ri'
-import { NowPlaying } from './NowPlaying'
 
 interface ExternalLinkProps {
   href: string
@@ -20,7 +19,6 @@ const ExternalLink = ({ href, children }: ExternalLinkProps) => (
 // eslint-disable-next-line import/prefer-default-export
 export const Footer = () => (
   <footer className="flex flex-col flex-shrink-0 justify-center items-center text-lg">
-    <NowPlaying />
     <div className="flex space-x-7 p-6 max-w-2xl mx-auto items-center">
       <ExternalLink href="http://twitter.com/adrserr10">
         <RiTwitterFill />
