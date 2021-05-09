006b8febe775dc3ed6479ec887f2d5577a56ab05
Adrián Serrano <aserrano@ea.com>
2021-05-09 21:08:30 +0200

    Framer motion animation dark mode

--- components/Container.tsx
+++ components/Container.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'react-i18next'
+import { motion, useAnimation } from 'framer-motion'
 import { Footer } from './Footer'
 import { LanguageSelect } from './LanguageSelect'
 
@@ -30,6 +31,7 @@ export function Container(props: ContainerProps) {
   const { theme, setTheme } = useTheme()
   const router = useRouter()
   const { t } = useTranslation('common')
+  const controls = useAnimation()
 
   // https://github.com/pacocoursey/next-themes#usetheme
   React.useEffect(() => setMounted(true), [])
@@ -117,7 +119,12 @@ export function Container(props: ContainerProps) {
             type="button"
             aria-label="Theme"
             className="h-10 w-10 p-3 bg-gray-200 dark:bg-gray-700 rounded-md mr-3"
-            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            onClick={async () => {
+              await controls.start({
+                rotate: [0, 360]
+              })
+              setTheme(theme === 'light' ? 'dark' : 'light')
+            }}
             disabled={!mounted}
           >
             {mounted && (
@@ -127,12 +134,16 @@ export function Container(props: ContainerProps) {
                 fill="currentColor"
               >
                 {theme === 'light' ? (
-                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+                  <motion.path
+                    d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+                    animate={controls}
+                  />
                 ) : (
-                  <path
+                  <motion.path
                     fillRule="evenodd"
                     d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
                     clipRule="evenodd"
+                    animate={controls}
                   />
                 )}
               </svg>
